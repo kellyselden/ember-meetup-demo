fa31c9ce98d9ce97491c49679ef5e303f3cdfc1c
Kelly Selden <kellyselden@gmail.com>
2017-02-08 08:04:11 -0800

    use curried macro

--- app/macros/add.js
+++ app/macros/add.js
@@ -1,15 +1,7 @@
-import computed from 'ember-macro-helpers/computed';
+import curriedComputed from 'ember-macro-helpers/curried-computed';
 
-export default function(key1, key2/*, keyN... */) {
-  // This is where you can inspect the incoming keys for errors.
+export default curriedComputed((val1, val2/*, valN... */) => {
+  // This is where your normal computed code will go.
   // For example:
-  if (arguments.length < 2) {
-    throw new Error('This macro needs two or more arguments.');
-  }
-
-  return computed(key1, key2/*, keyN... */, (val1, val2/*, valN... */) => {
-    // This is where your normal computed code will go.
-    // For example:
-    return val1 + val2;
-  }).readOnly();
-}
+  return val1 + val2;
+});
